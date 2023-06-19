cd3a2f9452379edb0667ab936edad7f72ea897c8
RomanSavushkin <romarsa82@gmail.com>
2023-06-19 20:06:48 +0300

    hw#7

--- src/telran/numbers/Calculator.java
+++ src/telran/numbers/Calculator.java
@@ -1,9 +1,26 @@
 package telran.numbers;
 
-public class Calculator {
-	static public double calculate(CalcData cd) {
-		// TODO
+import java.util.function.BinaryOperator;
 
-		return 0;
+public class Calculator {
+	public static double calculate(CalcData cd) {
+//		double num1 = cd.op1;
+//		double num2 = cd.op2;
+//		char oper = cd.operation;
+
+		int maxAsciiCode = Math.max(Math.max('+', '-'), Math.max('/', '*'));
+		DoubleBinaryOperator[] operations = new DoubleBinaryOperator[maxAsciiCode + 1];
+
+		operations['+'] = (a, b) -> a + b;
+		operations['-'] = (a, b) -> a - b;
+		operations['*'] = (a, b) -> a * b;
+		operations['/'] = (a, b) -> a / b;
+
+		return operations[(int) cd.operation].apply(cd.op1, cd.op2);
+
+	}
+
+	interface DoubleBinaryOperator extends BinaryOperator<Double> {
+		
 	}
 }
