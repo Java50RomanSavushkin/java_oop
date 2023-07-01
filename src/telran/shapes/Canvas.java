f1feae2cff759437de0c44145d0c130a6cd8176b
RomanSavushkin <romarsa82@gmail.com>
2023-07-01 17:44:15 +0300

    hw-10 solutions

--- src/telran/shapes/Canvas.java
+++ src/telran/shapes/Canvas.java
@@ -1,44 +1,68 @@
 package telran.shapes;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
-
+ 
 public class Canvas implements Shape, Iterable<Shape> {
 	private Shape[] shapes = new Shape[0];
 
 	private class CanvasIterator implements Iterator<Shape> {
+		int currentIndex = 0;
+		boolean flNext = false;
 
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
+
+			return currentIndex < shapes.length;
 		}
 
 		@Override
 		public Shape next() {
-			// TODO Auto-generated method stub
-			return null;
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			flNext = true;
+			return shapes[currentIndex++];
 		}
 
 		@Override
 		public void remove() {
-			// TODO
+			if (!flNext) {
+				throw new IllegalStateException();
+			}
+			flNext = false;
+			removeShapeAtIndex(--currentIndex);
 		}
 
-	};
-
-	@Override
-	public int perimeter() {
-		// TODO Auto-generated method stub
-		// sum of perimaters values for all shapes in this canvas
-		return 0;
 	}
 
 	@Override
+	public int perimeter() {
+		int res = 0;
+		for (Shape shape : this) {
+			res += shape.perimeter();
+		}
+		return res;
+	}
+
+	public void removeShapeAtIndex(int index) {
+		Shape[] tmp = new Shape[shapes.length - 1];
+		System.arraycopy(shapes, 0, tmp, 0, index);
+		System.arraycopy(shapes, index + 1, tmp, index, tmp.length - index);
+		shapes = tmp;
+
+	}
+ 
+	@Override
 	public int square() {
-		// TODO Auto-generated method stub
 		// sum of square values for all shapes in this canvas
-		return 0;
+		int res = 0;
+		for (Shape shape : this) {
+			res += shape.square();
+		}
+		return res;
 	}
 
 	@Override
@@ -48,14 +72,24 @@ public class Canvas implements Shape, Iterable<Shape> {
 	}
 
 	public void addShape(Shape shape) {
-		// TODO
+		shapes = Arrays.copyOf(shapes, shapes.length + 1);
+		shapes[shapes.length - 1] = shape;
 	}
 
 	public boolean removeIf(Predicate<Shape> predicate) {
-		// TODO
-		return false;
+		int oldLength = shapes.length;
+		Iterator<Shape> it = iterator();
+		while (it.hasNext()) {
+			Shape shape = it.next();
+			if (predicate.test(shape)) {
+				it.remove();
+			}
+		}
+		return oldLength > shapes.length;
 	}
+
 	public boolean removeNestedCanvases() {
-		return removeIf(shape-> shape instanceof Canvas);
+		return removeIf(shape -> shape instanceof Canvas);
 	}
+
 }
